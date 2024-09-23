fd5d69ea9e06ff2a2b9e41909457b31e671535f1
rakechen-0307 <rakechen168@gmail.com>
2024-09-23 10:32:54 +0800

    training test

--- rakec/clap.py
+++ rakec/clap.py
@@ -4,14 +4,12 @@ from audiocraft.models.musicgen import MusicGen, MusicGenCLAP
 from audiocraft.data.audio import audio_write
 from audiocraft.data.audio_utils import convert_audio
 
-model = MusicGenCLAP.get_pretrained('checkpoints/clapemb(ytcharts-v1)')
+model = MusicGenCLAP.get_pretrained('checkpoints/clapemb(fma-v1)')
 model.set_generation_params(duration=30, cfg_coef=0)
 
 sample_dir = "/work/u2614323/code/audiocraft-origin/samples"
 sample_files = sorted(os.listdir(sample_dir))
-wav_files = []
-for i in range(len(sample_files)):
-    wav_files.append(sample_dir + "/" + sample_files[i])
+wav_files = [sample_dir + "/" + sample_files[0]]
 print(wav_files)
 
 wavs = []
