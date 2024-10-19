42efea4b6025ff57faaabfdb66df3652e416aabc
rakechen-0307 <rakechen168@gmail.com>
2024-10-19 21:56:07 +0800

    spotify v1

--- rakec/clap.py
+++ rakec/clap.py
@@ -4,7 +4,7 @@ from audiocraft.models.musicgen import MusicGen, MusicGenCLAP
 from audiocraft.data.audio import audio_write
 from audiocraft.data.audio_utils import convert_audio
 
-model = MusicGenCLAP.get_pretrained('checkpoints/clapemb(fma-v2)')
+model = MusicGenCLAP.get_pretrained('checkpoints/clapemb(spotify-v1)')
 model.set_generation_params(duration=30, cfg_coef=0)
 
 sample_dir = "/work/u2614323/code/audiocraft-origin/samples"
