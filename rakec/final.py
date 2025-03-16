f7dfe89ad14c0e5d0d79486287d60fbf23a5e763
rakechen-0307 <rakechen168@gmail.com>
2025-03-16 11:51:58 +0800

    spotify-small inference

---
+++ rakec/final.py
@@ -0,0 +1,9 @@
+import os
+import torchaudio
+from audiocraft.models.musicgen import MusicGenCLAP
+from audiocraft.data.audio import audio_write
+from audiocraft.data.audio_utils import convert_audio
+
+model = MusicGenCLAP.get_pretrained('checkpoints/clapemb(spotify-small)')
+model.set_generation_params(duration=10, cfg_coef=3.0)
+
