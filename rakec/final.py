d6a333a3014323a1ca72903c0f81836781a808ba
rakechen-0307 <rakechen168@gmail.com>
2025-03-17 19:27:56 +0800

    spotify-small inference

--- rakec/final.py
+++ rakec/final.py
@@ -126,7 +126,7 @@ def video_to_frame(video_file):
     return frames
 
 
-music_model = MusicGenCLAP.get_pretrained('checkpoints/clapemb(spotify-small)')
+music_model = MusicGenCLAP.get_pretrained('checkpoints/clapemb(spotify-small-80)')
 music_model.set_generation_params(duration=30, cfg_coef=3.0)
 clap_conditioner = music_model.lm.condition_provider
 
