64fcbc3134d7a617a06991ff5d12b0e86031dc7a
rakechen-0307 <rakechen168@gmail.com>
2025-03-17 19:05:17 +0800

    spotify-small inference

--- audiocraft/modules/conditioners.py
+++ audiocraft/modules/conditioners.py
@@ -1101,11 +1101,13 @@ class CLAPEmbeddingConditioner(JointEmbeddingConditioner):
         """
         with torch.no_grad():
             wav = self._preprocess_wav(wav, length, sample_rates)
+            print(wav)
             B, T = wav.shape
             if T >= self.clap_max_frames:
                 wav = wav.unfold(-1, self.clap_max_frames, self.clap_stride)  # [B, F, T]
             else:
                 wav = wav.view(-1, 1, T)  # [B, F, T] with F=1
+            print(wav)
             wav = einops.rearrange(wav, 'b f t -> (b f) t')
             embed_list = []
             for i in range(0, wav.size(0), self.batch_size):
