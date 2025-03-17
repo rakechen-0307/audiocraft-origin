dc3df0c14154fe4e9dd08209f4503d52215f837a
rakechen-0307 <rakechen168@gmail.com>
2025-03-17 19:40:05 +0800

    spotify-small inference

--- rakec/final.py
+++ rakec/final.py
@@ -166,7 +166,7 @@ for i in range(len(files)):
             audio_embed = clipclap_model(frames)
     
     audio_embed = audio_embed.cpu()
-    null_condition = torch.zeros(1, 1, sample_rate*seg_length)
+    null_condition = torch.zeros(1, sample_rate*seg_length)
     null_embed = clap_conditioner.clap.get_audio_embedding_from_data(null_condition, use_tensor=True)
 
     print(f"audio embed: {audio_embed.shape}")
